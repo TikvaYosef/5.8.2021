90f4ce072635d60c83c75d4c76b14061c7e093ef
TikvaYosef <tikvayosef13@gmail.com>
2021-08-05 21:06:10 +0300

    1

---
+++ main.js
@@ -0,0 +1,132 @@
+// תרגיל 1
+
+// var age = [18, 20, 25, 63, 3, 120, 4, 21];
+
+// var min = age[0]
+// for (var i = 1; i < age.length; i++) {
+//     if (age[i] < min) {
+//         min=age[i]
+//     }
+
+// }
+// console.log(min)
+
+// var max = age[0]
+// for(var i = 1; i<age.length;i++){
+//     if(age[i]>max){
+//         max=age[i]
+//     }
+// }
+// console.log(max)
+
+
+// var sum = 0
+// for (var i = 1; i < age.length; i++) {
+//     sum = sum + age[i]
+// }
+// console.log(sum)
+
+
+// var avrge = 0
+
+// for (var i = 0; i < age.length; i++) {
+//     avrge = sum / age.length
+
+
+// }
+// console.log(avrge)
+
+
+// var pro = Number(prompt("guss the number"));
+// for (var i = 0; i < age.length; i++) {
+//     if (pro == age[i]) {
+//         console.log("yesss!!")
+//     }
+
+// }
+
+
+// var sum = 0
+// var maarach = []
+// for (var i = 0; i < 5; i++) {
+//     var userInput = Number(prompt("number"))
+//     maarach[i] = userInput
+
+
+
+
+// }
+// console.log(maarach)
+
+
+// for (i = 0; i < maarach.length; i++) {
+//     sum = sum + maarach[i]
+
+// }
+// console.log(sum)
+
+
+
+
+
+
+
+
+// תרגיל 3
+// var some = [1, 2, 3, 4, 5, 6, 7, 8]
+// var a = some[0]
+// var b = some[1]
+//  some[0]=b
+//   some[1]=a
+//  console.log(some)
+//  var c = some[7]
+//  some [0]=c
+//  some[7]=a
+//  console.log(some)
+
+
+
+// תרגיל 4
+
+// var num = [5, 3, 7, 8, 11]
+// var sum = 0
+// for (var i = 0; i < num.length; i++) {
+//     sum = sum + num[i]
+// }
+// console.log(sum)
+
+
+
+// var avrge =0
+// console.log( sum / num.length)
+
+
+
+// תרגיל 5
+
+
+
+// var Name = ["tikva", "keren", "yafit", "eden", "yosef"]
+// for (var i = 0; i < 5; i++) {
+   
+// }
+// console.log(Name)
+
+
+// תרגיל 6
+
+
+var Name = []
+for(var i=0; i<5;i++){
+    Name = prompt("enter here")
+}
+console.log(Name)
+
+
+
+
+
+
+
+
+// ["tikva", "keren", "yafit", "eden", "yosef","salomon","hilu","tesama"]
